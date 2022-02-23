6b4f663b965a4a173b1f6a179839550db39a06a6
İbrahim Can Erdoğan <52867508+icanerdogan@users.noreply.github.com>
2022-02-23 14:22:25 +0300

    coroutines

--- app/src/main/java/com/icanerdogan/retrofitkotlin/service/CryptoAPI.kt
+++ app/src/main/java/com/icanerdogan/retrofitkotlin/service/CryptoAPI.kt
@@ -2,11 +2,16 @@ package com.icanerdogan.retrofitkotlin.service
 
 import com.icanerdogan.retrofitkotlin.model.Crypto
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface CryptoAPI {
 
     @GET("assets/?apikey=8A010544-B3AC-45BD-A1F8-298803C8C64B")
-    fun getAllData(): Call<List<Crypto>>
+    // COROUTINES SONRASI
+    suspend fun getAllData(): Response<List<Crypto>>
+    // COROUTINES ÖNCESİ
+    //fun getAllData(): Call<List<Crypto>>
+
 
 }
